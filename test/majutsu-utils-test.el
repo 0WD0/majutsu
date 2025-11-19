722fb2553f524499af46050099bd1f5d95d3195d
0WD0 <wd.1105848296@gmail.com>
2025-11-19 13:30:37 +0800

    add misc test

---
+++ test/majutsu-utils-test.el
@@ -0,0 +1,32 @@
+;;; majutsu-utils-test.el --- Tests for majutsu utility functions  -*- lexical-binding: t; -*-
+
+(require 'ert)
+(require 'majutsu)
+
+(ert-deftest majutsu-utils-normalize-id-value ()
+  "Test ID normalization for various input types."
+  (should (equal (majutsu--normalize-id-value "abc") "abc"))
+  (should (equal (majutsu--normalize-id-value 'abc) "abc"))
+  (should (equal (majutsu--normalize-id-value 123) "123"))
+  (should (equal (majutsu--normalize-id-value nil) nil))
+  (let ((s "prop"))
+    (put-text-property 0 1 'face 'bold s)
+    (should (equal (majutsu--normalize-id-value s) "prop"))
+    (should-not (text-properties-at 0 (majutsu--normalize-id-value s)))))
+
+(ert-deftest majutsu-utils-diff-file-status ()
+  "Test file status inference from diff lines."
+  (should (equal (majutsu--diff-file-status '("new file mode 100644")) "new file"))
+  (should (equal (majutsu--diff-file-status '("deleted file mode 100644")) "deleted"))
+  (should (equal (majutsu--diff-file-status '("rename from old.txt" "rename to new.txt")) "renamed"))
+  (should (equal (majutsu--diff-file-status '("copy from source.txt" "copy to dest.txt")) "copied"))
+  (should (equal (majutsu--diff-file-status '("index 832...912 100644")) "modified")))
+
+(ert-deftest majutsu-utils-extract-bookmark-names ()
+  "Test bookmark name extraction from command output."
+  (should (equal (majutsu--extract-bookmark-names "bookmark: main") '("main")))
+  (should (equal (majutsu--extract-bookmark-names "bookmark: feature-1\nbookmark: feature-2") '("feature-1" "feature-2")))
+  (should (equal (majutsu--extract-bookmark-names "no bookmarks here") nil))
+  (should (equal (majutsu--extract-bookmark-names "bookmark: dev, bookmark: test") '("dev" "test"))))
+
+(provide 'majutsu-utils-test)
