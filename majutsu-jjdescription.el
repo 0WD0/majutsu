15ded31ba822644358a1f7f1f8269ed05942d699
0WD0 <wd.1105848296@gmail.com>
2026-01-02 00:25:57 +0800

    remove some with-editor config

--- majutsu-jjdescription.el
+++ majutsu-jjdescription.el
@@ -10,7 +10,24 @@
 
 ;;; Code:
 
+(require 'majutsu-jj)
+(require 'majutsu-mode)
+(require 'majutsu-process)
 
+(require 'with-editor)
+
+(defconst majutsu-jjdescription-regexp
+  (rx (seq (or string-start
+               (seq (* (not (any ?\n)))
+                    (any ?/ ?\\)))
+           "editor-" (+ (in "0-9A-Za-z"))
+           ".jjdescription" string-end))
+  "Regexp matching temporary jj description files created for editing.")
+
+(add-to-list 'with-editor-file-name-history-exclude majutsu-jjdescription-regexp)
+
+(add-to-list 'with-editor-server-window-alist
+             (cons majutsu-jjdescription-regexp #'pop-to-buffer-same-window))
 
 ;;; _
 (provide 'majutsu-jjdescription)
