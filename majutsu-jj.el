d193a3156bb6f8c0a55961d0274320429cd6d4d9
0WD0 <wd.1105848296@gmail.com>
2026-01-01 23:23:05 +0800

    add majutsu-jj.el and majutsu-jjdescription.el

---
+++ majutsu-jj.el
@@ -0,0 +1,47 @@
+;;; majutsu-jj.el --- JJ functionality  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2026 0WD0
+
+;; Author: 0WD0 <wd.1105848296@gmail.com>
+;; Maintainer: 0WD0 <wd.1105848296@gmail.com>
+;; Keywords: tools, vc
+;; URL: https://github.com/0WD0/majutsu
+
+;;; Commentary:
+
+;; This library provides early utilities and section subclasses that
+;; other Majutsu modules rely on while avoiding heavier dependencies.
+
+;;; Code:
+
+(require 'majutsu-base)
+
+;;; Options
+
+(defcustom majutsu-with-editor-envvar "JJ_EDITOR"
+  "Environment variable used to tell jj which editor to invoke."
+  :type 'string
+  :group 'majutsu)
+
+(defmacro majutsu-with-editor (&rest body)
+  "Like `with-editor*' but let-bind some more variables.
+Also respect the value of `majutsu-with-editor-envvar'."
+  (declare (indent 0) (debug (body)))
+  `(let ((majutsu-process-popup-time -1))
+     (with-editor* majutsu-with-editor-envvar
+       ,@body)))
+
+(defcustom majutsu-jj-executable "jj"
+  "Path to jj executable."
+  :group 'majutsu-process
+  :type 'string)
+
+(defcustom majutsu-jj-global-arguments
+  `("--no-pager" "--color=always")
+  :group 'majutsu-commands
+  :group 'majutsu-process
+  :type '(repeat string))
+
+;;; _
+(provide 'majutsu-jj)
+;;; majutsu-jj.el ends here
