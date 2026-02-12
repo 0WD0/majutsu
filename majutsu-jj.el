444ac78a048b2dcb89a92db3a3a8cd48811779e0
0WD0 <wd.1105848296@gmail.com>
2026-02-13 02:49:58 +0800

    feat: support tramp

--- majutsu-jj.el
+++ majutsu-jj.el
@@ -36,6 +36,13 @@
   :group 'majutsu-process
   :type 'string)
 
+(defcustom majutsu-remote-jj-executable "jj"
+  "Path to jj executable on remote hosts.
+When `default-directory' is remote, this executable is used instead of
+`majutsu-jj-executable'."
+  :group 'majutsu-process
+  :type 'string)
+
 (defcustom majutsu-jj-global-arguments
   `("--no-pager" "--color=always")
   "List of global arguments to pass to jj commands."
@@ -94,6 +101,41 @@ If TARGET is nil, return config for EDITOR-COMMAND as-is."
   (let ((command (append (or editor-command (majutsu-jj--editor-command-from-env))
                          (when target (list target)))))
     (majutsu-jj--toml-array-config key command)))
+
+(defsubst majutsu-jj--executable ()
+  "Return local or remote jj executable for `default-directory'."
+  (if (file-remote-p default-directory)
+      majutsu-remote-jj-executable
+    majutsu-jj-executable))
+
+(defun majutsu-convert-filename-for-jj (filename)
+  "Convert FILENAME so it can be passed to a jj subprocess.
+This follows Magit's `magit-convert-filename-for-git' direction: absolute
+remote file names are converted to host-local paths before being passed in
+`process-file'/`start-file-process' ARGS. Relative names are left untouched."
+  (if (not (file-name-absolute-p filename))
+      filename
+    (let ((expanded (expand-file-name filename)))
+      (or (file-remote-p expanded 'localname)
+          expanded))))
+
+(defun majutsu-jj-expand-filename-from-jj (path &optional directory)
+  "Expand PATH returned by jj into an Emacs-usable file name.
+If PATH is an absolute local path and DIRECTORY is remote, prepend the
+remote prefix from DIRECTORY so the result remains remote."
+  (let* ((base (or directory default-directory))
+         (remote (file-remote-p base)))
+    (cond
+     ((file-remote-p path)
+      (expand-file-name path))
+     ((and remote (file-name-absolute-p path))
+      (concat remote path))
+     (t
+      (expand-file-name path base)))))
+
+(defun majutsu-jj-expand-directory-from-jj (path &optional directory)
+  "Like `majutsu-jj-expand-filename-from-jj', but always return a directory."
+  (file-name-as-directory (majutsu-jj-expand-filename-from-jj path directory)))
 
 ;;; Reading
 
@@ -390,8 +432,9 @@ in the revision identifier (used for recursive refinement)."
 (define-error 'majutsu-jj-executable-not-found "jj executable cannot be found")
 
 (defun majutsu--assert-usable-jj ()
-  (unless (executable-find majutsu-jj-executable)
-    (signal 'majutsu-jj-executable-not-found (list majutsu-jj-executable)))
+  (let ((jj (majutsu-jj--executable)))
+    (unless (executable-find jj t)
+      (signal 'majutsu-jj-executable-not-found (list jj))))
   nil)
 
 (defun majutsu--not-inside-repository-error ()
@@ -425,22 +468,18 @@ trailing slash) or nil if not inside a JJ workspace."
         (with-temp-buffer
           (let ((coding-system-for-read 'utf-8-unix)
                 (coding-system-for-write 'utf-8-unix)
-                (exit (apply #'process-file majutsu-jj-executable nil t nil args)))
-            ;; `process-file' may return nil on success for some Emacs builds.
-            (when (null exit)
-              (setq exit 0))
+                (exit (apply #'process-file (majutsu-jj--executable) nil t nil args)))
             (when (zerop exit)
               (let ((out (string-trim (buffer-string))))
                 (unless (string-empty-p out)
-                  (file-name-as-directory (expand-file-name out)))))))))))
+                  (majutsu-jj-expand-directory-from-jj out default-directory))))))))))
 
 (defun majutsu--toplevel-safe (&optional directory)
   "Return the workspace root for DIRECTORY or signal an error."
   (or (majutsu-toplevel directory)
       (let ((default-directory
              (or (majutsu--safe-default-directory directory)
-                 (file-name-as-directory
-                  (expand-file-name (or directory default-directory))))))
+                 (majutsu-jj-expand-directory-from-jj (or directory default-directory)))))
         (majutsu--not-inside-repository-error))))
 
 (defmacro majutsu-with-toplevel (&rest body)
@@ -475,14 +514,11 @@ functions."
          (coding-system-for-read 'utf-8-unix)
          (coding-system-for-write 'utf-8-unix)
          (err-file (and return-error
-                        (make-temp-file "majutsu-jj-err")))
+                        (make-nearby-temp-file "majutsu-jj-err")))
          exit-code)
-    (majutsu--debug "Running command: %s %s" majutsu-jj-executable (string-join args " "))
-    (setq exit-code (apply #'process-file majutsu-jj-executable nil
+    (majutsu--debug "Running command: %s %s" (majutsu-jj--executable) (string-join args " "))
+    (setq exit-code (apply #'process-file (majutsu-jj--executable) nil
                            (list t err-file) nil args))
-    ;; process-file may return nil on success for some Emacs builds.
-    (when (null exit-code)
-      (setq exit-code 0))
     (majutsu--debug "Command completed in %.3f seconds, exit code: %d"
                     (float-time (time-subtract (current-time) start-time))
                     exit-code)
@@ -574,16 +610,13 @@ error text.  Output is optionally colorized based on
   (declare (indent 2))
   (setq args (majutsu-process-jj-arguments args))
   (let ((beg (point))
-        (exit (apply #'process-file majutsu-jj-executable nil t nil args)))
+        (exit (apply #'process-file (majutsu-jj--executable) nil t nil args)))
     (when (and (bound-and-true-p majutsu-process-apply-ansi-colors)
                (> (point) beg))
       ;; Use text-properties instead of overlays so that subsequent
       ;; washing/parsing that uses `buffer-substring' preserves faces.
       (let ((ansi-color-apply-face-function #'ansi-color-apply-text-property-face))
         (ansi-color-apply-on-region beg (point))))
-    ;; `process-file' may return nil on success for some Emacs builds.
-    (when (null exit)
-      (setq exit 0))
     (cond
      ;; Command produced no output.
      ((= (point) beg)
