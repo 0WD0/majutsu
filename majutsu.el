f585ca85ca080a3f0d36ba9bf2a0d01fc1c95906
0WD0 <wd.1105848296@gmail.com>
2026-05-01 14:33:32 +0800

    Add the Gerrit upload command.
    * NEWS.org: Mention Gerrit upload support.
    * docs/majutsu.org: Document the upload transient.
    * majutsu-gerrit.el: New file.
    (majutsu-gerrit--start): New function.
    (majutsu-gerrit-remote-branch-history): New variable.
    (majutsu-gerrit-upload--read-revset): New function.
    (majutsu-gerrit-upload--read-remote-branch): New function.
    (majutsu-gerrit-upload-arguments): New function.
    (majutsu-gerrit-upload): New command.
    (majutsu-gerrit-upload--repo-args): New function.
    (majutsu-gerrit-upload:--revision): New selection argument with a point toggle.
    (majutsu-gerrit-upload:--remote-branch): New argument.
    (majutsu-gerrit-upload:--remote): New argument.
    (majutsu-gerrit-upload:--reviewer): New argument.
    (majutsu-gerrit-upload:--cc): New argument.
    (majutsu-gerrit-upload:--label): New argument.
    (majutsu-gerrit-upload:--topic): New argument.
    (majutsu-gerrit-upload:--hashtag): New argument.
    (majutsu-gerrit-upload:--message): New argument.
    (majutsu-gerrit-upload:--notify): New argument.
    (majutsu-gerrit-upload:--deadline): New argument.
    (majutsu-gerrit-upload:--custom): New argument.
    (majutsu-gerrit-upload:--trace): New argument.
    (majutsu-gerrit-upload-transient): New transient.
    * majutsu-git.el (majutsu-git-transient): Add the Gerrit upload suffix.
    * majutsu.el: Require majutsu-gerrit.
    * test/majutsu-gerrit-test.el: New file.
    
    Closes #50.

--- majutsu.el
+++ majutsu.el
@@ -120,7 +120,8 @@ Instead of invoking this alias for `majutsu-log' using
   (require 'majutsu-op)
   (require 'majutsu-evolog)
   (require 'majutsu-workspace)
-  (require 'majutsu-conflict))
+  (require 'majutsu-conflict)
+  (require 'majutsu-gerrit))
 
 (with-eval-after-load 'evil
   (require 'majutsu-evil nil t))
