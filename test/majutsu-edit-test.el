7b606e697f4422ca5c69b7275e6b301b031cf048
0WD0 <wd.1105848296@gmail.com>
2026-06-15 12:04:11 +0800

    Move diffedit code out of edit module.
    * NEWS.org: Mention the diffedit module split.
    * docs/majutsu.org: Update diffedit documentation.
    * majutsu-diffedit.el: New file.
    (majutsu-diffedit-finish-on-save): New custom option.
    (majutsu-diffedit--root): New function.
    (majutsu-diffedit--finish-on-save): New function.
    (majutsu-diffedit--maybe-enable-mode): New function.
    (majutsu-diffedit--range): New function.
    (majutsu-diffedit--build-args): New function.
    (majutsu-diffedit--editor-target): New function.
    (majutsu-diffedit--file-at-point): New function.
    (majutsu-diffedit--read-file): New function.
    (majutsu-diffedit--normalize-file): New function.
    (majutsu-diffedit-run-with-editor): New function.
    * majutsu-edit.el (majutsu-edit-finish-on-save): Move to
    `majutsu-diffedit-finish-on-save'.
    (majutsu-edit--diffedit-root): Move to `majutsu-diffedit--root'.
    (majutsu-edit--finish-on-save): Move to `majutsu-diffedit--finish-on-save'.
    (majutsu-edit--maybe-enable-diffedit-mode): Move to
    `majutsu-diffedit--maybe-enable-mode'.
    (majutsu-edit--edit-range): Move to `majutsu-diffedit--range'.
    (majutsu-edit--build-diffedit-args): Move to
    `majutsu-diffedit--build-args'.
    (majutsu-edit--diffedit-editor-target): Move to
    `majutsu-diffedit--editor-target'.
    (majutsu-edit--file-at-point): Move to `majutsu-diffedit--file-at-point'.
    (majutsu-edit--read-diffedit-file): Move to `majutsu-diffedit--read-file'.
    (majutsu-edit--normalize-diffedit-file): Move to
    `majutsu-diffedit--normalize-file'.
    (majutsu-edit--run-diffedit): Move to `majutsu-diffedit-run-with-editor'.
    * majutsu-ediff.el (majutsu-ediff--run-diffedit): Use `majutsu-diffedit'.
    * majutsu.el: Require `majutsu-diffedit'.
    * test/majutsu-diffedit-test.el: New file.
    (majutsu-diffedit-test-root-detects-instructions): New test.
    (majutsu-diffedit-test-finish-on-save-calls-with-editor): New test.
    (majutsu-diffedit-test-run-with-editor-normalizes-absolute-file): New
    test.
    (majutsu-diffedit-test-run-with-editor-rejects-outside-absolute-file): New
    test.
    (majutsu-diffedit-test-build-args-keeps-files-separate): New test.
    * test/majutsu-edit-test.el (majutsu-edit-test-diffedit-root-detects-instructions): Move test.
    (majutsu-edit-test-finish-on-save-calls-with-editor): Move test.
    (majutsu-edit-test-run-diffedit-normalizes-absolute-file): Move test.
    (majutsu-edit-test-run-diffedit-rejects-outside-absolute-file): Move test.
    (majutsu-edit-test-build-diffedit-args-keeps-files-separate): Move test.
    * test/majutsu-ediff-test.el (majutsu-ediff-test-build-diffedit-args-keeps-files-separate): Update for the diffedit module.

--- test/majutsu-edit-test.el
+++ test/majutsu-edit-test.el
@@ -4,41 +4,13 @@
 
 ;;; Commentary:
 
-;; Tests for edit and diffedit helpers.
+;; Tests for changeset edit helpers.
 
 ;;; Code:
 
 (require 'ert)
 (require 'cl-lib)
 (require 'majutsu-edit)
-
-(ert-deftest majutsu-edit-test-diffedit-root-detects-instructions ()
-  "Diffedit root should be detected by JJ-INSTRUCTIONS file."
-  (let ((root (make-temp-file "majutsu-diffedit" t)))
-    (unwind-protect
-        (progn
-          (write-region "" nil (expand-file-name "JJ-INSTRUCTIONS" root) nil 'silent)
-          (let* ((nested (expand-file-name "right/file.txt" root))
-                 (dir (file-name-directory nested)))
-            (make-directory dir t)
-            (write-region "" nil nested nil 'silent)
-            (should (equal (file-name-as-directory root)
-                           (file-name-as-directory
-                            (majutsu-edit--diffedit-root nested))))))
-      (delete-directory root t))))
-
-(ert-deftest majutsu-edit-test-finish-on-save-calls-with-editor ()
-  "Finish-on-save should call with-editor-finish when active."
-  (let ((called nil))
-    (with-temp-buffer
-      (setq-local server-buffer-clients '(dummy))
-      (with-editor-mode 1)
-      (let ((majutsu-edit-finish-on-save t))
-        (cl-letf (((symbol-function 'with-editor-finish)
-                   (lambda (&optional _force)
-                     (setq called t))))
-          (majutsu-edit--finish-on-save))))
-    (should called)))
 
 (ert-deftest majutsu-edit-test-edit-changeset-prefers-literal-revision-at-point ()
   "Edit changeset should prefer the literal revision/ref under point."
@@ -56,50 +28,6 @@
                (lambda (&rest _args) nil)))
       (majutsu-edit-changeset)
       (should (equal captured '("edit" "main@origin"))))))
-
-(ert-deftest majutsu-edit-test-run-diffedit-normalizes-absolute-file ()
-  "Diffedit should normalize absolute file targets to repo-relative paths."
-  (let (seen-target seen-args seen-default)
-    (cl-letf (((symbol-function 'majutsu--toplevel-safe)
-               (lambda (&optional _directory) "/tmp/repo/"))
-              ((symbol-function 'majutsu-jj--editor-command-config)
-               (lambda (_key target &optional _editor-command)
-                 (setq seen-target target)
-                 "CFG"))
-              ((symbol-function 'majutsu-run-jj-async)
-               (lambda (&rest args)
-                 (setq seen-default default-directory)
-                 (setq seen-args args)
-                 0)))
-      (let ((default-directory "/tmp/repo/test/"))
-        (majutsu-edit--run-diffedit
-         '("--from" "a-" "--to" "a")
-         "/tmp/repo/test/majutsu-file-test.el")))
-    (should (equal seen-default "/tmp/repo/"))
-    (should (equal seen-target "$right/test/majutsu-file-test.el"))
-    (should (equal seen-args
-                   '("diffedit" "--config" "CFG"
-                     "--from" "a-" "--to" "a"
-                     "--" "file:\"test/majutsu-file-test.el\"")))))
-
-(ert-deftest majutsu-edit-test-run-diffedit-rejects-outside-absolute-file ()
-  "Diffedit should reject absolute file targets outside repository."
-  (cl-letf (((symbol-function 'majutsu--toplevel-safe)
-             (lambda (&optional _directory) "/tmp/repo/"))
-            ((symbol-function 'majutsu-run-jj-async)
-             (lambda (&rest _args)
-               (ert-fail "Should not execute diffedit for outside path"))))
-    (let ((default-directory "/tmp/repo/"))
-      (should-error
-       (majutsu-edit--run-diffedit
-        '("--from" "a-" "--to" "a")
-        "/tmp/other/file.el")
-       :type 'user-error))))
-
-(ert-deftest majutsu-edit-test-build-diffedit-args-keeps-files-separate ()
-  "Diffedit option args should not include file filters."
-  (should (equal (majutsu-edit--build-diffedit-args "a-" "a")
-                 '("--from" "a-" "--to" "a"))))
 
 (provide 'majutsu-edit-test)
 ;;; majutsu-edit-test.el ends here
