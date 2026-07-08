4b7de18b5ae68d70634111bfd864102567fe09d9
0WD0 <wd.1105848296@gmail.com>
2026-07-08 21:50:33 +0800

    Delete tags via majutsu-delete-thing.
    * majutsu-base.el (majutsu--confirm-actions): Add tag-delete.
    * majutsu-tag.el (majutsu-tag-section-map): Remap majutsu-delete-thing to
    majutsu-tag-delete.
    (majutsu-tag-delete): Confirm interactive deletions.
    * test/majutsu-tag-test.el
    (majutsu-tag-delete/interactive-confirms-selection): New test.

--- majutsu-base.el
+++ majutsu-base.el
@@ -82,6 +82,7 @@ which in turn uses the function specified here."
   '((const undo)
     (const redo)
     (const abandon)
+    (const tag-delete)
     (const git-remote-remove)
     (const rebase)
     (const workspace-forget))
