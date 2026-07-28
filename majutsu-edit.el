3a311e5e3c0b94212798087a144377483782b188
Arthur Heymans <arthur@aheymans.xyz>
2026-07-28 16:57:34 +0200

    Add optional Embark actions for bookmarks and workspaces
    Expose completion context and repository directories to Embark actions,
    including workspace root reuse and reusable revision editing commands.
    Document the integration and add comprehensive tests.

--- majutsu-edit.el
+++ majutsu-edit.el
@@ -19,6 +19,18 @@
 (require 'majutsu-process)
 
 ;;;###autoload
+(defun majutsu-edit-revision (revset &optional arg)
+  "Edit REVSET.
+With prefix ARG, pass --ignore-immutable.  Return non-nil on success."
+  (interactive (list (majutsu-read-revset "Edit revision")
+                     current-prefix-arg))
+  (when (zerop (apply #'majutsu-run-jj
+                      (append (list "edit" revset)
+                              (when arg (list "--ignore-immutable")))))
+    (message "Now editing commit %s" revset)
+    t))
+
+;;;###autoload
 (defun majutsu-edit-changeset (&optional arg)
   "Edit commit at point.
 
@@ -28,11 +40,8 @@ When called from a blob buffer, also visit the workspace file."
   (let ((in-blob (and (bound-and-true-p majutsu-blob-mode)
                       majutsu-buffer-blob-root
                       majutsu-buffer-blob-path)))
-    (if-let* ((revset (majutsu-revision-at-point))
-              (args (append (list "edit" revset)
-                            (when arg (list "--ignore-immutable")))))
-        (when (zerop (apply #'majutsu-run-jj args))
-          (message "Now editing commit %s" revset)
+    (if-let* ((revset (majutsu-revision-at-point)))
+        (when (majutsu-edit-revision revset arg)
           (when in-blob
             (let ((file (expand-file-name majutsu-buffer-blob-path
                                           majutsu-buffer-blob-root)))
